4c419012a8e763e9f859648ce84f1e7b4f74eedd
Marco <marcoolivera096@gmail.com>
2023-02-16 20:15:40 -0300

    quinto commit

--- 3D.py
+++ 3D.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
 
 fig = plt.figure()
-eixos = fig.add_subplot(projection = '3D')
+eixos = fig.add_subplot(projection = '3d')
 x,y,z =  axes3d.get_test_data(0.10)
 
 eixos.plot_wireframe(x,y,z)
